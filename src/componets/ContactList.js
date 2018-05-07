aa502eb57a2f1af51b1c8502c550c010a2d15982
bmcgrane302 <bmcgrane302@yahoo.com>
2018-05-07 14:09:49 -0700

    search bar

--- src/componets/ContactList.js
+++ src/componets/ContactList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Container, Row, Col } from 'reactstrap';
+import {Container, Row, Col, Input, InputGroup } from 'reactstrap';
 import Contact from './Contact';
 import TopNav from './TopNav';
 
 
 class ContactList extends Component {
 
+  state = {
+    filterTitle: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({filterTitle: e.target.value})
+  }
+
   render () {
-    let theContacts = this.props.contacts.map((contact,i) => {
+    console.log('state', this.state)
+    let theContacts = this.props.contacts.sort((a,b)=> b.id-a.lname).map((contact,i) => {
       return  (
         <Col key={contact.id} md={4}>
             <Contact contact={contact}/>
@@ -19,6 +28,13 @@ class ContactList extends Component {
     return (
      <div>
        <TopNav />
+       <InputGroup>
+         <Input
+           onChange={this.handleChange}
+           type= 'search'
+           placeholder='search for name'
+            />
+       </InputGroup>
        <Container>
          <Row>
            {theContacts}
